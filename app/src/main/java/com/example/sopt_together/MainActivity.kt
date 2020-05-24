a19d113053798dff8612ef62a90c94cf06a75800
JiHyeonMon <jhk8806@naver.com>
2020-05-24 11:32:24 +0900

    상단 만들기 + bottomnavigation구현(-ing)

--- app/src/main/java/com/example/sopt_together/MainActivity.kt
+++ app/src/main/java/com/example/sopt_together/MainActivity.kt
@@ -2,11 +2,45 @@ package com.example.sopt_together
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main_viewPager.adapter = MainPagerAdapter(supportFragmentManager)
+        main_viewPager.offscreenPageLimit = 5
+        main_viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
+            override fun onPageScrollStateChanged(state: Int) {
+
+            }
+
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                bottomNavigationView.menu.getItem(position).isChecked = true
+            }
+        })
+
+        bottomNavigationView.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+
+                R.id.menu_store -> main_viewPager.currentItem = 0
+                R.id.menu_class -> main_viewPager.currentItem = 1
+                R.id.menu_search -> main_viewPager.currentItem = 2
+                R.id.menu_category -> main_viewPager.currentItem = 3
+                R.id.menu_user -> main_viewPager.currentItem = 4
+            }
+            true
+        }
+
     }
 }
