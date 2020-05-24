5ec042526c851952e60cb4259b746a9938cf241f
JiHyeonMon <jhk8806@naver.com>
2020-05-25 05:40:12 +0900

    JiHyeon-store에 recyclerView구현 + Dummydata 넣기

--- app/src/main/java/com/example/sopt_together/main/MainActivity.kt
+++ app/src/main/java/com/example/sopt_together/main/MainActivity.kt
@@ -1,8 +1,9 @@
-package com.example.sopt_together
+package com.example.sopt_together.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.viewpager.widget.ViewPager
+import com.example.sopt_together.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,19 +12,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        main_viewPager.adapter = MainPagerAdapter(supportFragmentManager)
+        main_viewPager.adapter =
+            MainPagerAdapter(
+                supportFragmentManager
+            )
         main_viewPager.offscreenPageLimit = 5
         main_viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener{
-            override fun onPageScrollStateChanged(state: Int) {
-
-            }
+            override fun onPageScrollStateChanged(state: Int) {  }
 
             override fun onPageScrolled(
                 position: Int,
                 positionOffset: Float,
                 positionOffsetPixels: Int
-            ) {
-            }
+            ) {   }
 
             override fun onPageSelected(position: Int) {
                 bottomNavigationView.menu.getItem(position).isChecked = true
@@ -32,10 +33,8 @@ class MainActivity : AppCompatActivity() {
 
         bottomNavigationView.setOnNavigationItemSelectedListener {
             when(it.itemId){
-
                 R.id.menu_store -> main_viewPager.currentItem = 0
                 R.id.menu_category -> main_viewPager.currentItem = 3
-//                지현 수정
             }
             true
         }
--- app/src/main/java/com/example/sopt_together/main/MainPagerAdapter.kt
+++ app/src/main/java/com/example/sopt_together/main/MainPagerAdapter.kt
@@ -1,8 +1,10 @@
-package com.example.sopt_together
+package com.example.sopt_together.main
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import com.example.sopt_together.fragment.category.CategoryFragment
+import com.example.sopt_together.ui.fragment.store.StoreFragment
 
 class MainPagerAdapter (fm:FragmentManager) :
         FragmentPagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT){
